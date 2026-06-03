115482cd7e48eaefafb32c300a688ed18ec41bdd
Cii-Nsr <nursuciramadani239@gmail.com>
2026-06-03 17:00:49 +0800

    Add files via upload

---
+++ kasir/js/products.js
@@ -0,0 +1,94 @@
+// Data produk dengan gambar (gunakan URL gambar online sebagai contoh)
+window.ProductsData = [
+  { 
+    id: 1, 
+    name: "Nasi Goreng Spesial", 
+    category: "makanan", 
+    price: 18000, 
+    imageUrl: "images/makanan-3.jpg"
+  },
+  { 
+    id: 2, 
+    name: "Mie Ayam Bakso", 
+    category: "makanan", 
+    price: 15000, 
+    imageUrl: "images/makanan-4.jpg"
+  },
+  { 
+    id: 3, 
+    name: "Sate Ayam (8 tusuk)", 
+    category: "makanan", 
+    price: 13000, 
+    imageUrl: "images/makanan-7.jpg"
+  },
+  { 
+    id: 4, 
+    name: "Es Teh Manis", 
+    category: "minuman", 
+    price: 5000, 
+    imageUrl: "images/minuman-1.jpg"
+  },
+  { 
+    id: 5, 
+    name: "Es Kopi Susu", 
+    category: "minuman", 
+    price: 12000, 
+    imageUrl: "images/minuman-2.jpg"
+  },
+  { 
+    id: 6, 
+    name: "Jus Alpukat", 
+    category: "minuman", 
+    price: 15000, 
+    imageUrl: "images/minuman-4.jpg"
+  },
+  { 
+    id: 7, 
+    name: "Air Mineral", 
+    category: "minuman", 
+    price: 5000, 
+    imageUrl: "images/minuman-6.jpg"
+  }, 
+  { 
+    id: 8, 
+    name: "Bakso", 
+    category: "makanan", 
+    price: 12000, 
+    imageUrl: "images/makanan-5.jpg"
+  },
+  {
+    id: 9, 
+    name: "Bakso Mercon", 
+    category: "makanan", 
+    price: 15000, 
+    imageUrl: "images/makanan-1.jpg"
+  },
+  { 
+    id: 10, 
+    name: "Seblak", 
+    category: "makanan", 
+    price: 12000, 
+    imageUrl: "images/makanan-6.jpg"
+  },
+  { 
+    id: 11, 
+    name: "Mie Ayam", 
+    category: "makanan", 
+    price: 12000, 
+    imageUrl: "images/makanan-2.jpg"
+  },
+  {
+    id: 12, 
+    name: "Jus Buah Naga", 
+    category: "minuman", 
+    price: 13000, 
+    imageUrl: "images/minuman-5.jpg"
+  },
+  { 
+    id: 13, 
+    name: "Jus Mangga", 
+    category: "minuman", 
+    price: 13000, 
+    imageUrl: "images/minuman-3.jpg"
+  }
+];
